eeed2d8d81411bcdb3edcdee2cf09191436c498f
Eddie Montoya <eisaiahmo22@gmail/com>
2024-04-08 17:41:35 -0600

    changed the table 2 colums

--- db/schema.sql
+++ db/schema.sql
@@ -6,8 +6,5 @@ USE radlibs_db;
 
 CREATE TABLE radlibs (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    adjective VARCHAR(255),
-    noun VARCHAR(255),
-    verb VARCHAR(255),
-    adverb VARCHAR(255)
+    user_input VARCHAR(255);
 );

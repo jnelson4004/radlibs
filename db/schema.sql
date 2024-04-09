1dede09de99196ee45669499531ce0b99eaee8ff
Chelle Wood <chelleyoungw@gmail.com>
2024-04-08 19:30:40 -0500

    removed code lines 7-10 on schema.sql, redundant code

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE radlibs_db;
 
 USE radlibs_db;
 
-CREATE TABLE radlibs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_input VARCHAR(255);
-);
+-- CREATE TABLE radlibs (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     user_input VARCHAR(255);
+-- );

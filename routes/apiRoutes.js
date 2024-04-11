0f8a5121e0f092897e096fd37db6fa384d7fedf3
burt reynolds <jnelson4004@gmail.com>
2024-04-10 21:16:15 -0500

    multiple pages

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,27 @@
 const router = require('express').Router();
 
 router.post('/', async (req, res) => {
-  console.log('hello')
+  console.log('hello');
   });
 
 router.get("/", async (req, res) => {
-  res.render('login');
-    console.log("eat my jorts")
+  res.render("login");
+    console.log("eat my jorts");
+  });
+
+  router.get("/radlib", async (req, res) => {
+    res.render("radlib");
+    console.log("i'm in your area");
+  });
+
+  router.get("/login", async (req, res) => {
+    res.render("login");
+    console.log("i am going to sell your information");
+  });
+
+  router.get("/createaccount", async (req, res) => {
+    res.render("createaccount");
+    console.log("holy crap")
   });
 
 
